6d72327db83e249ed1302ca761086b5514ca42bd
iman_masroori <imanmasrori@yahoo.com>
2020-11-13 00:12:00 +0330

    Design Game Board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Mensch Game with Tkinter Module in Python, Code by Iman Masroori
+import random
+
 
 class Piece:
     def __init__(self):
@@ -9,7 +11,18 @@ class Piece:
 
 
 class gBox():
+    all_count = 4
+
     def __init__(self):
+        pass
+
+    def at_home(self):
+        pass
+
+    def in_game(self):
+        pass
+
+    def out_game(self):
         pass
 
 
@@ -27,3 +40,16 @@ class rBox():
     def __init__(self):
         pass
 
+
+def roll():
+    return random.randint(1, 6)
+
+
+def turn():
+    turn_list = iter([1, 2, 3, 4])
+    return next(turn_list)
+print("ddd",turn())
+
+
+list_circle_board = [0]
+print(roll())
