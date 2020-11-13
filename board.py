040d14c04a1a1337b305905a09b6fec2e1d752a6
iman_masroori <imanmasrori@yahoo.com>
2020-11-13 13:03:39 +0330

    resolved Conflicts

--- board.py
+++ board.py
@@ -27,56 +27,56 @@ can_board = Canvas(frame_right, width=750, height=660)
 can_board.pack()
 
 
-can_board.create_oval(20, 40, 80, 100)
-can_board.create_oval(670, 40, 730, 100)
-can_board.create_oval(20, 550, 80, 610)
-can_board.create_oval(670, 550, 730, 610)
+# can_board.create_oval(20, 40, 80, 100)
+# can_board.create_oval(670, 40, 730, 100)
+# can_board.create_oval(20, 550, 80, 610)
+# can_board.create_oval(670, 550, 730, 610)
 
-lbl_blue = Label(can_board, image=photo_blue_player)
-lbl_blue.place(x=32, y=46)  # x + 12  y+6
-lbl_num_blue = Label(can_board, text="4", fg="white", bg="#010080")
-lbl_num_blue.place(x=42, y=70)  # x+22 y+30
+# lbl_blue = Label(can_board, image=photo_blue_player)
+# lbl_blue.place(x=32, y=46)  # x + 12  y+6
+# lbl_num_blue = Label(can_board, text="4", fg="white", bg="#010080")
+# lbl_num_blue.place(x=42, y=70)  # x+22 y+30
+#
+# lbl_red = Label(can_board, image=photo_red_player)
+# lbl_red.place(x=682, y=46)
+# lbl_num_red = Label(can_board, text="4", fg="white", bg="red")
+# lbl_num_red.place(x=692, y=70)
+#
+# lbl_green = Label(can_board, image=photo_green_player)
+# lbl_green.place(x=32, y=556)
+# lbl_num_green = Label(can_board, text="4", fg="white", bg="green")
+# lbl_num_green.place(x=42, y=580)
+#
+# lbl_yellow = Label(can_board, image=photo_yellow_player)
+# lbl_yellow.place(x=682, y=556)
+# lbl_num_yellow = Label(can_board, text="4", fg="white", bg="yellow")
+# lbl_num_yellow.place(x=692, y=580)
 
-lbl_red = Label(can_board, image=photo_red_player)
-lbl_red.place(x=682, y=46)
-lbl_num_red = Label(can_board, text="4", fg="white", bg="red")
-lbl_num_red.place(x=692, y=70)
-
-lbl_green = Label(can_board, image=photo_green_player)
-lbl_green.place(x=32, y=556)
-lbl_num_green = Label(can_board, text="4", fg="white", bg="green")
-lbl_num_green.place(x=42, y=580)
-
-lbl_yellow = Label(can_board, image=photo_yellow_player)
-lbl_yellow.place(x=682, y=556)
-lbl_num_yellow = Label(can_board, text="4", fg="white", bg="yellow")
-lbl_num_yellow.place(x=692, y=580)
-
-for i in range(40, 220, 80):
-    can_board.create_oval(250, i, 310, i + 60)
-    if i < 180:
-        can_board.create_oval(325, i, 385, i + 60)
-
-for i in range(390, 570, 80):
-    can_board.create_oval(250, i, 310, i + 60)
-    if i > 430:
-        can_board.create_oval(325, i, 385, i + 60)
-
-for i in range(40, 220, 80):
-    can_board.create_oval(400, i, 460, i + 60)
-
-for i in range(390, 570, 80):
-    can_board.create_oval(400, i, 460, i + 60)
-
-for i in range(20, 200, 80):
-    can_board.create_oval(i, 280, i + 60, 340)
-    if i < 110:
-        can_board.create_oval(i, 280, i + 60, 340)
-
-for i in range(570, 750, 80):
-    can_board.create_oval(i, 210, i + 60, 270)
-    if i > 430:
-        can_board.create_oval(325, i, 385, i + 60)
+# for i in range(40, 220, 80):
+#     can_board.create_oval(250, i, 310, i + 60)
+#     if i < 180:
+#         can_board.create_oval(325, i, 385, i + 60)
+#
+# for i in range(390, 570, 80):
+#     can_board.create_oval(250, i, 310, i + 60)
+#     if i > 430:
+#         can_board.create_oval(325, i, 385, i + 60)
+#
+# for i in range(40, 220, 80):
+#     can_board.create_oval(400, i, 460, i + 60)
+#
+# for i in range(390, 570, 80):
+#     can_board.create_oval(400, i, 460, i + 60)
+#
+# for i in range(20, 200, 80):
+#     can_board.create_oval(i, 280, i + 60, 340)
+#     if i < 110:
+#         can_board.create_oval(i, 280, i + 60, 340)
+#
+# for i in range(570, 750, 80):
+#     can_board.create_oval(i, 210, i + 60, 270)
+#     if i > 430:
+#         can_board.create_oval(325, i, 385, i + 60)
 
 # for i in range(40,220,80):
 #     can_board.create_oval(400, i, 460, i+60)
